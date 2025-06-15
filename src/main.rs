a372acfd53788337c6b9883400fc210f2aa9353f
rcurran <rcurran@alumni.bentley.edu>
2025-06-15 09:28:18 -0400

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use axum::{routing::get, Router};
+use axum::{Router, routing::get};
+use serde::Deserialize;
+use std::fs;
 use std::net::SocketAddr;
-
-async fn hello() -> &'static str {
-    "Hello, world!"
-}
+use toml::de::from_str;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() {
+async fn main()  -> Result<(), Box<dyn Error>>{
+    // load the old config up
+    let config : Config = match fs::read_to_string("config.toml") {
+        Ok(content) => from_str(&content).expect("unable to parse config into struct"),
+        Err(_) => panic!("cannot read config.toml!!"),
+    };
+
     // Build the application with a route
     let app = Router::new().route("/", get(hello));
 
     // Define the address to bind the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([127, 0, 0, 1], config.web_config.port));
     println!("Listening on http://{}", addr);
 
     // Run the server
@@ -19,9 +25,27 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
+async fn hello() -> &'static str {
+    "Hello, world!"
+}
+#[derive(Debug, Deserialize)]
+struct Config {
+    web_config: WebServerConfig,
+    topics: Vec<TopicConfig>,
+}
 
-// todos:
-// - design Rest apis and document
-//  - GET /comsume/topic 
+#[derive(Debug, Deserialize)]
+struct WebServerConfig {
+    port: u16,
+}
+
+#[derive(Debug, Deserialize)]
+struct TopicConfig {
+    name: String,
+    mmap: bool,
+    compression: bool,
+}
