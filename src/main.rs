16c2845f410d102675d7d7e7131455bd5fe9e314
rcurran <rcurran@alumni.bentley.edu>
2025-06-15 17:35:46 -0400

    progress

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,33 @@ use std::fs;
 use std::net::SocketAddr;
 use toml::de::from_str;
 use std::error::Error;
+use sled::{Db, Config};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main()  -> Result<(), Box<dyn Error>>{
     // load the old config up
-    let config : Config = match fs::read_to_string("config.toml") {
+    let config : BobConfig = match fs::read_to_string("config.toml") {
         Ok(content) => from_str(&content).expect("unable to parse config into struct"),
         Err(_) => panic!("cannot read config.toml!!"),
     };
+
+    let mut topic_db_map = HashMap::new();
+    // iterate over topics, create dbs if they don't exist
+    for topic in config.topics.iter() {
+        println!("found topic: {}", topic.name);
+        println!("enable compression: {}", topic.compression);
+        let config = Config::new().use_compression(topic.compression).path(topic.name.clone());
+        let db : Db = match config.open() {
+            Ok(db) => db,
+            Err(_) => panic!("unable to open db: {}", topic.name.clone())
+        };
+
+        if topic_db_map.contains_key(&topic.name) {
+            panic!("topic name: {} declared twice", topic.name)
+        }
+        topic_db_map.insert(topic.name.clone(), db);
+    }
 
     // Build the application with a route
     let app = Router::new().route("/", get(hello));
@@ -33,7 +52,7 @@ async fn hello() -> &'static str {
     "Hello, world!"
 }
 #[derive(Debug, Deserialize)]
-struct Config {
+struct BobConfig {
     web_config: WebServerConfig,
     topics: Vec<TopicConfig>,
 }
@@ -46,6 +65,5 @@ struct WebServerConfig {
 #[derive(Debug, Deserialize)]
 struct TopicConfig {
     name: String,
-    mmap: bool,
     compression: bool,
 }
