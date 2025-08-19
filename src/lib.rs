d9f87287f9dcef5231311a060ed23972f19e2519
bob <rcurran@alumni.bentley.edu>
2025-08-18 22:58:31 -0400

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -215,10 +215,6 @@ pub async fn start_web_server(config: BobConfig) -> Result<(), Box<dyn Error>> {
         )
         .route("/produce/{topic_name}", post(produce_handler))
         .route("/stats/{topic_name}", get(topic_stats_handler))
-        .route(
-            "/subscribe/{topic_name}/{consumer_id}",
-            get(subscribe_handler),
-        )
         .with_state(shared_state);
 
     // Run the server
@@ -238,17 +234,6 @@ pub async fn start_web_server(config: BobConfig) -> Result<(), Box<dyn Error>> {
 }
 
 async fn health() {}
-
-async fn subscribe_handler(
-    Path((topic_name, consumer_id)): Path<(String, String)>,
-    State(state): State<Arc<AppState>>,
-) -> impl IntoResponse {
-    // A `Stream` that repeats an event every second
-    let stream = stream::repeat_with(|| Ok::<_, axum::Error>(Event::default().data("hi!")));
-
-    Sse::new(stream).keep_alive(KeepAlive::default())
-}
-use axum::response::sse::{Event, KeepAlive, Sse}
 
 async fn topic_stats_handler(
     Path(topic_name): Path<String>,
