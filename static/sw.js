7cbcf206ba8f4a66ead32a275c9eaf0ccdae7c6b
bob <rcurran@alumni.bentley.edu>
2025-09-12 22:04:00 -0400

    ok

--- static/sw.js
+++ static/sw.js
@@ -1,16 +1,22 @@
 const CACHE_NAME = 'my-webpage-cache';
 
-// Install event handler
+// two ideas here
+// 1. i can access html from a browser cache if i am offline or server not reachable
+// 2. if i can access html but request failes due to network error, i can save items to local storage
+// ... one in local storage, figure out a way to broadcast them once connection restores
+// learn more about progessive web apps...
 self.addEventListener('install', (event) => {
   console.log("intall event");
   console.log(event);
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll([
+      .then((cache) => {
+        cache.addAll([
         '/', // cache root URL
         'static/dashboard.html',
-        'static/index.html',
-      ]))
+        '/static',
+      ]); console.log(cache);
+    }).catch((error) => console.log(error))
   );
 });
 
@@ -23,4 +29,19 @@ self.addEventListener('fetch', (event) => {
       return response || fetch(event.request);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
